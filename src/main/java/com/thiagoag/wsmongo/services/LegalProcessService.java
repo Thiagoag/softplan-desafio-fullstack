0696b7f940fb298b6ab8b552e6ab9fe4ab78bd5a
Thiago Garcia <thiago_garcia@outlook.com>
2018-08-25 16:58:58 -0300

    Getting process by id

---
+++ src/main/java/com/thiagoag/wsmongo/services/LegalProcessService.java
@@ -0,0 +1,23 @@
+package com.thiagoag.wsmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagoag.wsmongo.domain.LegalProcess;
+import com.thiagoag.wsmongo.repository.LegalProcessRepository;
+import com.thiagoag.wsmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class LegalProcessService {
+
+	@Autowired
+	private LegalProcessRepository lpRepo;
+	
+	public LegalProcess findById(String id) {
+		Optional<LegalProcess> process = lpRepo.findById(id);
+		return process.orElseThrow(() -> new ObjectNotFoundException("Process not found"));
+	}
+	
+}
