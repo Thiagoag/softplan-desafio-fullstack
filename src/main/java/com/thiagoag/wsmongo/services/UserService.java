873d0581cdadef27e554f7aa4b13e29a219f1731
Thiago Garcia <thiago_garcia@outlook.com>
2018-08-25 14:22:37 -0300

    Get user by id

--- src/main/java/com/thiagoag/wsmongo/services/UserService.java
+++ src/main/java/com/thiagoag/wsmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.thiagoag.wsmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thiagoag.wsmongo.domain.User;
 import com.thiagoag.wsmongo.repository.UserRepository;
+import com.thiagoag.wsmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
 	}
 }
